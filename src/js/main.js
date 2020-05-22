01e4a8766fa4ffa535fd0cfe8ef62243d278041f
Eltonleao <3lton.leao@gmail.com>
2020-05-22 10:53:02 -0300

    testes com swal

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,9 @@
 import './../scss/style.scss';
+
+$('.mdl-navigation__link').click(function(){
+    swal('','teste','').then(function(){
+        swal('','outro gato','').then(function(){
+            swal('','boneca russa','');
+        })
+    })
+})
